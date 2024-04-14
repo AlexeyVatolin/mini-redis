46061855bce1a6f66046f9401bfa3d1becd3970d
Alexey Vatolin <vatolinalex@gmail.com>
2024-04-14 23:36:11 +0200

    Respond to multiple PINGs

--- app/main.py
+++ app/main.py
@@ -6,11 +6,16 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, addr = server_socket.accept()
-    conn.sendall(b"+PONG\r\n")
+    connection, addr = server_socket.accept()
+    with connection:
+        print(f"Connected by {addr}")
+        while True:
+            data = connection.recv(1024)
+            if not data:
+                break
+            connection.sendall(b"+PONG\r\n")
+        connection.close()
 
 
 if __name__ == "__main__":
